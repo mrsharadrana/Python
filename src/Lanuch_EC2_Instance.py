8587b660ebacc61e3198d32ba452738abe434de2
sharad <sharad@dell>
2020-06-06 00:13:51 +0530

    June 6

--- src/Lanuch_EC2_Instance.py
+++ src/Lanuch_EC2_Instance.py
@@ -1,6 +1,16 @@
-'''
-Created on 05-Jun-2020
+import boto3
 
-@author: sharad
+ec2 = boto3.resource('ec2')
 '''
-#First Install Boto3 :- pip install boto3
+instance = ec2.create_instances(
+    ImageId = 'ami-0b0b075706e19de29',
+    MinCount = 1,
+    MaxCount =1,
+    InstanceType = 't2.micro')
+
+print(instance[0].id)
+'''
+instance_id = 'i-00cd13acb9bde955a'
+instance = ec2.Instance(instance_id)
+response = instance.terminate()
+print(response)
